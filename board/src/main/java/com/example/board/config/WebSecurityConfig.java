0b1a8c400630ea14f6b487e63c466e1410e3196b
hanbit <ghiesf@naver.com>
2025-02-06 19:02:54 +0900

    del: JWT 토큰 적용 취소 및 Spring Security 설정 적용

--- board/src/main/java/com/example/board/config/WebSecurityConfig.java
+++ board/src/main/java/com/example/board/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.board.config;
 
-import com.example.board.filter.JwtAuthenticationFilter;
-import com.example.board.util.JwtTokenProvider;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -21,31 +17,31 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private JwtTokenProvider jwtTokenProvider;
-    private AuthenticationEntryPoint authenticationEntryPoint;
-
     @Bean
-    WebSecurityCustomizer configure() {
-        return web -> web.ignoring().requestMatchers(toH2Console()).requestMatchers("/static/**");
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring().requestMatchers(toH2Console()).requestMatchers("/static/**", "/css/**", "/js/**", "/media/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html");
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, JwtTokenProvider jwtTokenProvider) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf(auth -> auth
-                        .ignoringRequestMatchers("/api/user/signup", "/api/user/login")
-                        .ignoringRequestMatchers(PathRequest.toH2Console())
-                )
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/user/signup", "/api/user/login").permitAll()
-                            .requestMatchers(PathRequest.toH2Console()).permitAll()
-                            .anyRequest().authenticated()
+                        auth.requestMatchers("/", "/login", "/api/user/signup", "/api/article", "/api/article/{id}").permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .anyRequest().authenticated()
                 )
-//                .addFilterBefore(
-//                        new JwtAuthenticationFilter(jwtTokenProvider),
-//                        UsernamePasswordAuthenticationFilter.class
-//                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));;
+                .formLogin(auth -> auth
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/")
+                        .permitAll()
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("Login failed: " + exception.getMessage());
+                            response.sendRedirect("/");
+                        })
+                )
+                .csrf(auth -> auth.disable());
+
 
         return httpSecurity.build();
     }
