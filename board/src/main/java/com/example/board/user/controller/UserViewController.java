8ea315e69cfe38fa8576f2a0b529e4463f201243
hanbit <ghiesf@naver.com>
2025-02-07 15:53:08 +0900

    feat: 기능 동작 확인용 html(인덱스, 게시판, 마이페이지) 생성

--- board/src/main/java/com/example/board/user/controller/UserViewController.java
+++ board/src/main/java/com/example/board/user/controller/UserViewController.java
@@ -1,14 +1,20 @@
 package com.example.board.user.controller;
 
+import com.example.board.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserViewController {
+    private final UserService userService;
+
     @GetMapping("/login")
     public String login() {
         return "user/login";
@@ -18,5 +24,17 @@ public class UserViewController {
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(Model model) {
+        model.addAttribute("loginUserInfo", userService.getLoginUserInfo());
+        return "user/mypage";
+    }
+
+    @GetMapping("/signout")
+    public String signout(Model model) {
+        model.addAttribute("loginUserInfo", userService.getLoginUserInfo());
+        return "user/signout";
     }
 }
