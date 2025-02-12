41ecf98d40d0ccec86f8f276a1ab25e695558a25
hanbit <ghiesf@naver.com>
2025-02-12 14:00:10 +0900

    feat: 대시보드(게시글 전체 조회) 화면 개발

--- board/src/main/java/com/example/board/article/controller/ArticleViewController.java
+++ board/src/main/java/com/example/board/article/controller/ArticleViewController.java
@@ -1,22 +1,25 @@
 package com.example.board.article.controller;
 
+import com.example.board.article.service.ArticleService;
 import com.example.board.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ArticleViewController {
     private final UserService userService;
+    private final ArticleService articleService;
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model) {
+    public String dashboard(Model model, @RequestParam(required = false, defaultValue="0") int page) {
         model.addAttribute("authPrincipal", userService.getAuthenticationPrincipal());
-        model.addAttribute("loginUserInfo", userService.getLoginUserInfo());
+        model.addAttribute("pagedArticles", articleService.getArticleList(page));
         return "article/dashboard";
     }
 }
