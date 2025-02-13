944d66e1f039a02fdef5e7c18536f974a6c98298
biiit4894 <ghiesf@naver.com>
2025-02-13 22:45:41 +0900

    fix: 댓글 작성 및 수정 제약조건 설정

--- board/src/main/java/com/example/board/comment/model/dto/UpateCommentReqDto.java
+++ board/src/main/java/com/example/board/comment/model/dto/UpateCommentReqDto.java
@@ -1,10 +1,12 @@
 package com.example.board.comment.model.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UpateCommentReqDto {
+    @Size(min = 1, max = 500, message="1자 이상 500자 이하여야 합니다.")
     private String content;
 }
